29fb1341155e3995a9549cc5e299034c0c20d489
Egle Puodziunaite <Egle.Puodziunaite@ibm.com>
2020-02-26 22:38:28 +0200

    first commit

---
+++ src/main/java/mif/vu/ikeea/IkeeaApplication.java
@@ -0,0 +1,13 @@
+package mif.vu.ikeea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkeeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkeeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mif/vu/ikeea/IkeeaApplicationTests.java
@@ -0,0 +1,13 @@
+package mif.vu.ikeea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkeeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
