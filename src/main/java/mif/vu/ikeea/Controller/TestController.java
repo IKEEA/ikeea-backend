5c4fd35945d1b89b34ba386c5a21b162a65e07c9
avoitovas <andrejus.voitovas@gmail.com>
2020-03-08 14:29:26 +0200

    Initial DB commit

---
+++ src/main/java/mif/vu/ikeea/Controller/TestController.java
@@ -0,0 +1,32 @@
+package mif.vu.ikeea.Controller;
+
+import mif.vu.ikeea.Entity.Repository.UserRepository;
+import mif.vu.ikeea.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class TestController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
