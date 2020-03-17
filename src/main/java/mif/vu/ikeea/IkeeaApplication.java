59e8c1d945153325aebda78cb63b7f0d0cc683dd
ievavizg <ievaviz92@gmail.com>
2020-03-17 20:39:37 +0200

    Working on it

--- src/main/java/mif/vu/ikeea/IkeeaApplication.java
+++ src/main/java/mif/vu/ikeea/IkeeaApplication.java
@@ -3,11 +3,11 @@ package mif.vu.ikeea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class IkeeaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IkeeaApplication.class, args);
 	}
-
 }
