2d3935e709038d5a1826900f4bdd4d133ff80363
ievavizg <ievaviz92@gmail.com>
2020-03-27 20:08:17 +0200

    User CRUD operations

--- src/main/java/mif/vu/ikeea/Controller/TestController.java
+++ src/main/java/mif/vu/ikeea/Controller/TestController.java
@@ -1,32 +1,50 @@
 package mif.vu.ikeea.Controller;
 
+import mif.vu.ikeea.Entity.Team;
 import mif.vu.ikeea.Entity.User;
-import mif.vu.ikeea.Entity.Repository.UserRepository;
+import mif.vu.ikeea.Service.UserService;
+import mif.vu.ikeea.Service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @Controller
 @RequestMapping(path="/demo")
 public class TestController {
 
-        @Autowired
-        private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
-        @PostMapping(path = "/add")
-        public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email){
+    /*
+        @GetMapping(path = "/all")
+        public @ResponseBody Iterable<User> getAllUsers () {
+            return userRepository.findAll();
+
+        }
+        */
+
+    @PostMapping(path = "/addUser")
+    public @ResponseBody String addAnotherUser (@RequestParam String name, @RequestParam String email, @RequestParam String pssw){
         User n = new User();
-        userRepository.save(n);
+        n.setEmail(email);
+        n.setPassword(pssw);
+
+        Team team = new Team();
+        team.setTitle(name);
+
+        userService.addUser(n);
         return "Saved";
     }
 
-        @GetMapping(path = "/all")
-        public @ResponseBody Iterable<User> getAllUsers () {
-        return userRepository.findAll();
+    @GetMapping(path = "/getUsers")
+    public @ResponseBody List<User> getAllUsers (){
+        return userService.getAllUsers();
     }
 
+    @GetMapping(path = "/deleteUser")
+    public @ResponseBody String deleteUser (@RequestParam Long id){
+        userService.deleteUserById(id);
+        return "User deleted";
+    }
 }
