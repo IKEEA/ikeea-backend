1e0a9042b1dd1246a6623cc2d7760a4c5c02e893
avoitovas <andrejus.voitovas@gmail.com>
2020-03-29 17:02:55 +0300

    JWT Setup

--- src/main/java/mif/vu/ikeea/Entity/Repository/UserRepository.java
+++ src/main/java/mif/vu/ikeea/Entity/Repository/UserRepository.java
@@ -4,4 +4,5 @@ import mif.vu.ikeea.Entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
 }
