56564560bf00b159f6ac18c93a1f97fc9b095620
ievavizg <ievaviz92@gmail.com>
2020-04-19 17:44:01 +0300

    fixed User List response for manager

--- src/main/java/mif/vu/ikeea/Responses/UserManagerResponse.java
+++ src/main/java/mif/vu/ikeea/Responses/UserManagerResponse.java
@@ -3,6 +3,11 @@ package mif.vu.ikeea.Responses;
 import lombok.Getter;
 import lombok.Setter;
 import mif.vu.ikeea.Entity.ApplicationUser;
+import mif.vu.ikeea.Entity.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -12,13 +17,27 @@ public class UserManagerResponse {
     private String lastName;
     private String email;
     private Integer restrictionDays;
+    private Boolean enabled;
+    private List<String> roles;
 
     public UserManagerResponse(ApplicationUser applicationUser){
         this.id = applicationUser.getId();
         this.firstName = applicationUser.getFirstName();
         this.lastName = applicationUser.getLastName();
         this.email = applicationUser.getEmail();
+        this.roles = buildRolesName(applicationUser.getRoles());
+        this.enabled = applicationUser.getEnabled();
         this.restrictionDays = applicationUser.getRestrictionDays();
+    }
+
+    private List<String> buildRolesName(Set<Role> roles) {
+        List<String> roleNames = new ArrayList<>();
+
+        for (Role role : roles) {
+            roleNames.add(role.getName().toString());
+        }
+
+        return roleNames;
     }
 
 
