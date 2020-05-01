dce7d929ad0bb4b483f459761768e35f2fe5e353
avoitovas <andrejus.voitovas@gmail.com>
2020-05-01 17:41:35 +0300

    Remove any data related to team

--- src/main/java/mif/vu/ikeea/Factory/UserFactory.java
+++ src/main/java/mif/vu/ikeea/Factory/UserFactory.java
@@ -2,12 +2,11 @@ package mif.vu.ikeea.Factory;
 
 import mif.vu.ikeea.Entity.Role;
 import mif.vu.ikeea.Entity.ApplicationUser;
-import mif.vu.ikeea.Entity.Team;
 
 import java.util.Collections;
 
 public class UserFactory {
-    public static ApplicationUser createUser(String email, Role role, String password, ApplicationUser manager, Team team, String token) {
+    public static ApplicationUser createUser(String email, Role role, String password, ApplicationUser manager, String token) {
         ApplicationUser user = new ApplicationUser();
 
         user.setEmail(email);
@@ -15,7 +14,6 @@ public class UserFactory {
         user.setPassword(password);
         user.setManagerWithChild(manager);
         user.setToken(token);
-        user.setTeam(team);
 
         return user;
     }
--- src/main/java/mif/vu/ikeea/Manager/UserManager.java
+++ src/main/java/mif/vu/ikeea/Manager/UserManager.java
@@ -3,14 +3,12 @@ package mif.vu.ikeea.Manager;
 import mif.vu.ikeea.Entity.Repository.RoleRepository;
 import mif.vu.ikeea.Entity.Role;
 import mif.vu.ikeea.Entity.ApplicationUser;
-import mif.vu.ikeea.Entity.Team;
 import mif.vu.ikeea.Enums.ERole;
 import mif.vu.ikeea.Exceptions.*;
 import mif.vu.ikeea.Factory.UserFactory;
 import mif.vu.ikeea.Generator.TokenValueGenerator;
 import mif.vu.ikeea.Payload.RegistrationRequest;
 import mif.vu.ikeea.Payload.UpdateProfileRequest;
-import mif.vu.ikeea.RepositoryService.TeamService;
 import mif.vu.ikeea.RepositoryService.UserService;
 import mif.vu.ikeea.Responses.UserProfileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +23,6 @@ public class UserManager
     private UserService userService;
 
     @Autowired
-    private TeamService teamService;
-
-    @Autowired
     private RoleRepository roleRepository;
 
     @Autowired
@@ -37,7 +32,6 @@ public class UserManager
     public ApplicationUser create(String email, ApplicationUser manager) {
         String generatedPassword = TokenValueGenerator.generate();
         String password = passwordEncoder.encode(generatedPassword);
-        Team team = teamService.loadByManagerId(manager.getId());
 
         Role role = roleRepository.findByName(ERole.DEVELOPER)
                 .orElseThrow(() -> new ResourceNotFoundException("User Role not set."));
@@ -47,7 +41,6 @@ public class UserManager
                 role,
                 password,
                 manager,
-                team,
                 TokenValueGenerator.generate()
         );
 
