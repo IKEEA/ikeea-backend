f8c1ae9ed0579e27111354b33362c6ec18a1e32c
ievavizg <ievaviz92@gmail.com>
2020-05-10 15:17:15 +0300

    learning day filters

--- src/main/java/mif/vu/ikeea/Controller/LearningDayController.java
+++ src/main/java/mif/vu/ikeea/Controller/LearningDayController.java
@@ -6,24 +6,18 @@ import mif.vu.ikeea.Manager.LearningDayManager;
 import mif.vu.ikeea.Payload.LearningDayRequest;
 import mif.vu.ikeea.Payload.UpdateLearningDayRequest;
 import mif.vu.ikeea.RepositoryService.LearningDayService;
-import mif.vu.ikeea.RepositoryService.UserService;
 import mif.vu.ikeea.Responses.ApiResponse;
 import mif.vu.ikeea.Responses.LearningDayResponse;
 import mif.vu.ikeea.Specifications.LearningDaySpecification;
-import mif.vu.ikeea.Specifications.LearningDaySpecificationsBuilder;
-import mif.vu.ikeea.Specifications.SearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(path="/api/learning-day")
@@ -36,9 +30,6 @@ public class LearningDayController {
     private LearningDayService learningDayService;
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private LearningDayRepository learningDayRepository;
 
     @PostMapping("/add")
@@ -47,69 +38,16 @@ public class LearningDayController {
 
         return ResponseEntity.ok(new ApiResponse(true, "Learning day added successfully"));
     }
-/*
- //   @GetMapping(path = "/{managerId}/list")
- //   public @ResponseBody List<LearningDayResponse> getLearningDaysList(@PathVariable Long managerId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd")Date date) {
+
     @GetMapping(path = "/{managerId}/list")
-    public @ResponseBody List<LearningDayResponse> getLearningDaysList(@PathVariable Long managerId, @RequestParam(required = false) Long userId, @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date, @RequestParam(required = false) Long topicId) {
-        List<LearningDay> learningDays = new ArrayList<>();
+    public List<LearningDayResponse> getLearningDaysListSearch(@PathVariable Long managerId,
+                                                               @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date date,
+                                                               @RequestParam(required = false) Long userId, @RequestParam(required = false) Long topicId){
 
-        if(userId != null){
-            learningDays = learningDayService.getAllByManagerIdAndUserId(managerId, userId);
-        }
-        if (date != null){
-            learningDays = learningDayService.getAllByManagerIdAndDate(managerId, date);
-        }
-        if (topicId != null) {
-            learningDays = learningDayService.getAllByUserIdAndTopicId(managerId, topicId);
-        }
-        else {
-            learningDays = learningDayService.getAllByManagerId(managerId);
-        }
-
-        List<LearningDayResponse> learningDayResponses = new ArrayList<>();
-
-        for (LearningDay learningDay : learningDays) {
-            learningDayResponses.add(new LearningDayResponse(learningDay));
-        }
-
-        return learningDayResponses;
-    }
-*/
-
-    @GetMapping(path = "/{managerId}/listParameters")
-    public List<LearningDayResponse> getLearningDaysList(
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date date, @RequestParam(required = false) Long userId, @RequestParam(required = false) Long topicId) {
-
-        //LearningDaySpecification spec = new LearningDaySpecification(new SearchCriteria("date", ":", date));
-        LearningDaySpecification spec = new LearningDaySpecification(new SearchCriteria("user", ":", userId));
-        //LearningDaySpecification spec = new LearningDaySpecification(new SearchCriteria("topic", ":", topicId));
-
-        List<LearningDay> learningDays = learningDayRepository.findAll(spec);
-
-        List<LearningDayResponse> learningDayResponses = new ArrayList<>();
-
-        for (LearningDay learningDay : learningDays) {
-            learningDayResponses.add(new LearningDayResponse(learningDay));
-        }
-
-        return learningDayResponses;
-    }
-
-    @GetMapping(path = "/{managerId}/listSearch")
-    public List<LearningDayResponse> getLearningDaysList(
-            @RequestParam(value = "search") String search) {
-
-        LearningDaySpecificationsBuilder builder = new LearningDaySpecificationsBuilder();
-        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-        Matcher matcher = pattern.matcher(search + ",");
-        while (matcher.find()) {
-            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
-        }
-
-        Specification<LearningDay> spec = builder.build();
-        List<LearningDay> learningDays = learningDayRepository.findAll(spec);
-
+        List<LearningDay> learningDays = learningDayRepository.findAll(Specification.where(LearningDaySpecification.withManager(managerId))
+                .and(Specification.where(LearningDaySpecification.withDate(date)))
+                .and(Specification.where(LearningDaySpecification.withTopic(topicId)))
+                .and(Specification.where(LearningDaySpecification.withUser(userId))));
         List<LearningDayResponse> learningDayResponses = new ArrayList<>();
 
         for (LearningDay learningDay : learningDays) {
--- src/main/java/mif/vu/ikeea/Entity/Repository/TopicRepository.java
+++ src/main/java/mif/vu/ikeea/Entity/Repository/TopicRepository.java
@@ -2,8 +2,9 @@ package mif.vu.ikeea.Entity.Repository;
 
 import mif.vu.ikeea.Entity.LearningDay;
 import mif.vu.ikeea.Entity.Topic;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TopicRepository extends CrudRepository<Topic, Long> {
+public interface TopicRepository extends CrudRepository<Topic, Long>, JpaSpecificationExecutor<LearningDay>{
     Iterable<Topic> findAllByLearningDays(LearningDay learningDays);
 }
