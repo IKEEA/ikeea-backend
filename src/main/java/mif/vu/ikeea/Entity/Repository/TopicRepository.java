7d47db18d41daeaebb926bb570286d8d46056c1f
avoitovas <andrejus.voitovas@gmail.com>
2020-05-19 19:02:30 +0300

    Refactored code + added topic check

--- src/main/java/mif/vu/ikeea/Entity/Repository/TopicRepository.java
+++ src/main/java/mif/vu/ikeea/Entity/Repository/TopicRepository.java
@@ -4,8 +4,6 @@ import mif.vu.ikeea.Entity.LearningDay;
 import mif.vu.ikeea.Entity.Topic;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface TopicRepository extends CrudRepository<Topic, Long>{
     Iterable<Topic> findAllByLearningDays(LearningDay learningDays);
 
